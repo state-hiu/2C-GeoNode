29276eaf70bc35ceded1f4cea61a5e69234ab4f8
milafrerichs <mila.frerichs@gmail.com>
2017-08-21 11:46:03 -0400

    Add categories to a drop down menu
    use the TopiCCategories for now, there are additional categories, that
    ned to be added but can simply be appended in the context_processor
    the context_processor is needed to make the categories available to all
    templates
    the settings need to be updated with the current settings it does not
    work. the stripped down version from simod does work but I did not
    include them here since this would be a big change and need to be
    evaluated first

--- state_2c_geonode/settings.py
+++ state_2c_geonode/settings.py
@@ -1058,3 +1058,4 @@ try:
     from local_settings import *  # noqa
 except ImportError:
     pass
+TEMPLATES[0]['OPTIONS']['context_processors'].append('state_2c_geonode.context_processors.resource_urls')
