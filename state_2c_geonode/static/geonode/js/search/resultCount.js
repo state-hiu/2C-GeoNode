e214ae14385d0772ab1b49845e775b87a79c1ef4
milafrerichs <mila.frerichs@gmail.com>
2017-08-31 17:30:15 +0200

    Add function for checking if value is not defined or empty

--- state_2c_geonode/static/geonode/js/search/resultCount.js
+++ state_2c_geonode/static/geonode/js/search/resultCount.js
@@ -3,10 +3,15 @@ var module = angular.module('geonode_main_search');
 
 module.directive('resultCount', function(){
   return {
-    template: "<span style='font-size:34px;'>{{counts}} datasets found<span ng-hide='query == \"\"'> for &quot;{{query}}&quot;</span>",
+    template: "<span style='font-size:34px;'>{{counts}} datasets found<span ng-hide='hideQuery()'> for &quot;{{query}}&quot;</span>",
     scope: {
       counts: '=',
       query: '='
+    },
+    link: function(scope, elem) {
+      scope.hideQuery = function() {
+        return((!scope.query || scope.query === "") ? true : false);
+      };
     }
   };
 });
