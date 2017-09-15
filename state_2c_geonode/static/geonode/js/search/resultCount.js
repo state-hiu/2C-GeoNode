778971a7398dc9c9f1595e6091f9ea1e26e63d36
Lennin <lenninlasd@gmail.com>
2017-09-15 15:52:11 +0000

    Add the search controller to the 2C app in order to show the count message after the search

--- state_2c_geonode/static/geonode/js/search/resultCount.js
+++ state_2c_geonode/static/geonode/js/search/resultCount.js
@@ -1,18 +1,29 @@
 (function(){
 var module = angular.module('geonode_main_search');
 
-module.directive('resultCount', function(){
+module.directive('resultCount', ['$rootScope', function($rootScope){
   return {
-    template: "<span style='font-size:34px;'>{{counts}} datasets found<span ng-hide='hideQuery()'> for &quot;{{query}}&quot;</span>",
+    template: "<span style='font-size:34px;'>"    +
+	      "    {{counts_}} datasets found"     +
+	      "    <span ng-hide='hideQuery()'> for <i>{{query_}}</i>  </span>" +
+	      "</span>",
     scope: {
       counts: '=',
       query: '='
     },
     link: function(scope, elem) {
+      scope.query_ = scope.query;
+
       scope.hideQuery = function() {
-        return((!scope.query || scope.query === "") ? true : false);
+        return((!scope.query_ || scope.query_ === "") ? true : false);
       };
+
+      scope.$on('searchPerformed', function(e, data){
+	scope.query_ = data.meta.title__icontains;
+	scope.counts_ = data.meta.total_count;
+      });
     }
   };
-});
+
+}]);
 })();
