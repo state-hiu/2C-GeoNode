54c6de7eb4f60770c1feaae8c4b08c2c72fb4559
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2017-10-20 16:45:38 +0300

    Fix Data Provider Contact column

--- layer_override/models.py
+++ layer_override/models.py
@@ -9,5 +9,5 @@ class Layerextension(object):
     city = models.CharField(max_length=128,blank=True,null=True)
     dataprovider_name = models.CharField(max_length=128,blank=True,null=True)
     dataprovider_url = models.CharField(max_length=255, null=True, blank=True)
-    dataprovider_contact = models.CharField(max_length=128,blank=True,null=True) 
+    dataprovider_contact = models.CharField(max_length=255,blank=True,null=True)
     dataprovider_type = models.CharField(max_length=128,null=True)
